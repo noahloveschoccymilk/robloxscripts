ac93bd454f4643b16d1b311823370ae5b1394b95
noahloveschoccymilk <sku93@outlook.com>
2022-01-10 13:12:19 -0700

    Create robloxscriptthatsgood.lua

---
+++ robloxscriptthatsgood.lua
@@ -0,0 +1,26 @@
+local HeheFart = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+
+HeheFart.Name = "HeheFart"
+HeheFart.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+HeheFart.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = HeheFart
+Frame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.342025459, 0, 0.325153381, 0)
+Frame.Size = UDim2.new(0.315281957, 0, 0.349079758, 0)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.175033689, 0, 0.330404222, 0)
+TextLabel.Size = UDim2.new(0.651856065, 0, 0.339191556, 0)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "you're about to get banned. gg."
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
